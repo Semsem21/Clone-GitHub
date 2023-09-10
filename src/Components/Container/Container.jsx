df68f76a696bf20fd839ffa1a3fed9f04eada94b
Islam Mohamed <123085326+Semsem21@users.noreply.github.com>
2023-09-11 01:38:27 +0300

    Add files via upload

---
+++ src/Components/Container/Container.jsx
@@ -0,0 +1,8 @@
+function Container({ children }) {
+	return (
+		<div className="w-screen xl:px-20 md:px-8 sm:px-7 px-7 mx-auto">
+			{children}
+		</div>
+	);
+}
+export default Container;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {},
+			screens: {
+				ssm: "537px",
+				sssm: "380px",
+			},
+		},
+	},
+	plugins: [],
+};
